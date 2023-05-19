adbddfe8c26578741a68dd018394d4f9e61257b7
翊翊翊yi <319635848@qq.com>
2023-05-19 22:26:12 +0800

    0000

---
+++ 23-vue_child/src/components/childComponents.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" v-model="myMessage" @input="passData(myMessage)" />
+</template>
+<script>
+export default {
+  name: "childComponent",
+  data() {
+    return {
+      myMessage: this.message,
+    };
+  },
+  props: {
+    message: String,
+  },
+  methods: {
+    passData(val) {
+      this.$emit("getChildData");
+    },
+  },
+};
+</script>
---
+++ 23-vue_child/src/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>父子组件通信，父组件msg值</p>
+    <child-componet :message="msg" v-on:getChildData="getData"></child-componet>
+  </div>
+</template>
+<script>
+import childComponet from "./components/childComponents.vue";
+export default {
+  components: {
+    childComponet,
+  },
+  data(){
+    return{
+        msg: "测试一下",
+    };
+  },
+  methods:{
+    getData(val){
+        this.msg=val;
+    },
+  },
+};
+</script>
