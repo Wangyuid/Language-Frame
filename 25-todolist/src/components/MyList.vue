572acc0065e3d74d964f88908122f08427dbf32b
翊翊翊yi <319635848@qq.com>
2023-05-31 16:46:39 +0800

    0000

---
+++ 25-todolist/src/components/MyList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="todo-main">
+    <MyItem />
+  </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem.vue";
+export default {
+  name: "MyList",
+  components: {
+    MyItem,
+  },
+};
+</script>
+
+<style scope>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
